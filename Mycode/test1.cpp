ccc3b81715746f0d1f484995038342daa68bbd90
wudikaka <2012479454@qq.com>
2026-03-03 23:54:44 +0800

    重新写了点二分查找的题目

--- Mycode/test1.cpp
+++ Mycode/test1.cpp
@@ -5,37 +5,56 @@ using namespace std;
 class Solution
 {
 public:
-    bool isPalindrome(string s)
+    vector<int> searchRange(vector<int> &nums, int target)
     {
-        int l = 0;
-        int r = s.size() - 1;
-        while (l < r)
+        if (nums.empty())
         {
-            while (!isalnum(s[l]) && l < r)
-            {
-                ++l;
-            }
-            while (!isalnum(s[r]) && l < r)
-            {
-                --r;
-            }
-
-            if (s[l] < 'a')
-            {
-                s[l] += 32;
-            }
-            if (s[r] < 'a')
-            {
-                s[r] += 32;
-            }
-
-            if (s[r] != s[l])
-            {
-                return false;
-            }
-            ++l;
-            --r;
+            return {-1, -1};
         }
-        return true;
+
+        // 找头
+        int l = 0;
+        int r = nums.size() - 1;
+        int mid;
+        while (l <= r)
+        {
+            mid = l + (r - l) / 2;
+            if (nums[mid] < target)
+            {
+                l = mid + 1;
+            }
+            else if (nums[mid] >= target)
+            {
+                r = mid - 1;
+            }
+        }
+        // 头已经找到了，尾部分就不用判断了，尾部分最多重叠
+        if (l >= nums.size() || nums[l] != target)
+        {
+            return {-1, -1};
+        }
+        int first = l;
+
+        // 找尾
+        l = 0;
+        r = nums.size() - 1;
+        while (l <= r)
+        {
+            mid = l + (r - l) / 2;
+            if (nums[mid] <= target)
+            {
+                l = mid + 1;
+            }
+            else if (nums[mid] > target)
+            {
+                r = mid - 1;
+            }
+        }
+        int last = l - 1;
+
+        vector<int> my_target;
+        my_target.push_back(first);
+        my_target.push_back(last);
+        return my_target;
     }
 };
