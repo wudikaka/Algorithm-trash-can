0aed83c6507d470abcb7d0bc0a4b74e4e6f72c4a
wudikaka <2012479454@qq.com>
2026-03-26 01:43:27 +0800

    学了排序和前缀和部分，重新分类了一下文件夹

--- Mycode/test1.cpp
+++ Mycode/test1.cpp
@@ -1,74 +1,44 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-class Solution
+class NumMatrix
 {
 public:
-    string minWindow(string s, string t)
+    vector<vector<int>> sums;
+    NumMatrix(vector<vector<int>> &matrix)
     {
-        // 用数组代替哈希表（字符的 ASCII 范围是 0-127）
-        int need[128] = {0}; // 记录 t 中每个字符还需要多少
-        int needCount = 0;   // 记录 t 中不同字符的种类数
-
-        // 初始化 need 数组
-        for (char c : t)
+        int rowSize = matrix.size(); // 行数
+        if (rowSize > 0)
         {
-            if (need[c] == 0)
-                needCount++; // 遇到新字符，种类数+1
-            need[c]++;
-        }
-
-        int left = 0, right = 0;
-        int have[128] = {0}; // 记录当前窗口中有需要的字符的数量
-        int haveCount = 0;   // 记录当前窗口已经满足了多少种字符
-
-        int minLen = INT_MAX;
-        int start = 0; // 记录最短子串的起始位置
-
-        while (right < s.length())
-        {
-            char c = s[right];
-
-            // 如果这个字符是 t 需要的
-            if (need[c] > 0)
+            int colSize = matrix[0].size(); // 列数
+            sums.resize(rowSize, vector<int>(colSize + 1));
+            for (int i = 0; i < rowSize; i++)
             {
-                have[c]++;
-
-                // 如果这个字符的数量已经达到要求
-                if (have[c] == need[c])
+                for (int j = 0; j < colSize; j++) // 给每行都搞个前缀和数组
                 {
-                    haveCount++;
+                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
                 }
             }
-
-            // 当所有字符都满足要求时，尝试缩小左边界
-            while (haveCount == needCount && left <= right)
-            {
-                // 更新最小长度
-                if (right - left + 1 < minLen)
-                {
-                    minLen = right - left + 1;
-                    start = left;
-                }
-
-                // 移动左边界
-                char leftChar = s[left];
-                if (need[leftChar] > 0)
-                {
-                    if (have[leftChar] == need[leftChar])
-                    {
-                        haveCount--;
-                    }
-                    have[leftChar]--;
-                }
-                left++;
-            }
-
-            right++;
         }
+    }
 
-        return minLen == INT_MAX ? "" : s.substr(start, minLen);
+    int sumRegion(int row1, int col1, int row2, int col2)
+    {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++)
+        {
+            sum += (sums[i][col2 + 1] - sums[i][col1]);
+        }
+        return sum;
     }
 };
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
