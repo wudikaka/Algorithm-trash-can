864d85323617e9dc7fe8f28845b892f3a9703b18
wudikaka <2012479454@qq.com>
2026-03-30 22:48:02 +0800

    前缀和与差分收尾

--- Mycode/test1.cpp
+++ Mycode/test1.cpp
@@ -5,40 +5,12 @@
 #include <queue>
 using namespace std;
 
-class NumMatrix
+class Solution
 {
 public:
-    vector<vector<int>> sums;
-    NumMatrix(vector<vector<int>> &matrix)
+    vector<int> arrayRankTransform(vector<int> &arr)
     {
-        int rowSize = matrix.size(); // 行数
-        if (rowSize > 0)
-        {
-            int colSize = matrix[0].size(); // 列数
-            sums.resize(rowSize, vector<int>(colSize + 1));
-            for (int i = 0; i < rowSize; i++)
-            {
-                for (int j = 0; j < colSize; j++) // 给每行都搞个前缀和数组
-                {
-                    sums[i][j + 1] = sums[i][j] + matrix[i][j];
-                }
-            }
+        vector<int> temp = arr;
+        sort(temp.begin(), temp.end());
         }
-    }
-
-    int sumRegion(int row1, int col1, int row2, int col2)
-    {
-        int sum = 0;
-        for (int i = row1; i <= row2; i++)
-        {
-            sum += (sums[i][col2 + 1] - sums[i][col1]);
-        }
-        return sum;
-    }
 };
-
-/**
- * Your NumMatrix object will be instantiated and called as such:
- * NumMatrix* obj = new NumMatrix(matrix);
- * int param_1 = obj->sumRegion(row1,col1,row2,col2);
- */
--- Mycode/test2.cpp
+++ Mycode/test2.cpp
@@ -2,4 +2,35 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+
 using namespace std;
+
+typedef struct
+{
+    float realpart;
+    float imagpart;
+} Complex;
+
+void assign(Complex *A, float real, float imag);   /* 赋值 */
+void add(Complex *C, Complex *A, Complex *B);      /* A + B */
+void minus(Complex *A, float real, float imag);    /* A - B */
+void multiply(Complex *A, float real, float imag); /* A * B */
+void divide(Complex *A, float real, float imag);   /* A / B */
+
+void assign(Complex *A, float real, float imag)
+{
+    A->imagpart = imag;
+    A->realpart = real;
+}
+
+void add(Complex *C, Complex *A, Complex *B)
+{
+    C->imagpart = A->imagpart + B->imagpart;
+    C->realpart = A->realpart + B->realpart;
+}
+
+int main()
+{
+}
